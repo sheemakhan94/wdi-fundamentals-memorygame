3566df51bc5a170d7b4209297d173d0ae99b1df9
Sheema Khan <sheemakhan@sheemas-air.home>
2019-05-07 00:57:39 +0100

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,21 @@
-console.log("Up and running!");
-console.log("User flipped " + cardOne)
-console.log("User flipped " + cardTwo)
+
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push("cardOne");
-console.log("User flipped queen.")
-var cardTwo = cards[2];
-cardsInPlay.push("cardTwo");
-console.log("User flipped king.")
 
+function checkForMatch() {
 if (cardsInPlay[0] === cardsInPlay[1]) {
-	alert("You found a match!");
+	console.log("You found a match!");
 } else {
-	alert("Sorry, try again.");
-}
+	console.log("Sorry, try again.");
+}	
+};
+
+function flipCard(cardId) {
+	console.log("User flipped" + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
